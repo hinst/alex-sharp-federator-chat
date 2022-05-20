74f7026ff055d880463e98d89b357271aad96464
hinst <hinstance@yandex.ru>
2022-05-20 18:11:48 +0200

    Find sentence part

--- chatbot/src/components/Chat.tsx
+++ chatbot/src/components/Chat.tsx
@@ -44,11 +44,21 @@ export default class Chat extends Component {
         const chainingWord = chainingWords[Math.floor(Math.random() * chainingWords.length)];
         const chainingWordIndex = message1.indexOf(chainingWord);
 
-        const messages2 = MESSAGES.filter(message => message.includes(chainingWord) && message != message1 &&
+        let messages2 = MESSAGES.filter(message => message.includes(chainingWord) && message != message1 &&
             !(chainingWordIndex == 0 && message.indexOf(chainingWord) == 0)
         );
+        if (messages2.length == 0)
+            messages2 = MESSAGES;
         const message2 = messages2[Math.floor(Math.random() * messages2.length)];
-        console.log(message1, message2);
+
+        console.log(message1, chainingWord, message2);
+        if (message1.includes(chainingWord)) {
+            const [sentence1, direction1] = findSentencePart(message1, chainingWordIndex);
+            if (message2.includes(chainingWord)) {
+                const [sentence2, direction2] = findSentencePart(message2, message2.indexOf(chainingWord), (-1) * direction1);
+                console.log(sentence1, sentence2);
+            }
+        }
     }
 }
 
@@ -72,4 +82,31 @@ function getWords(text: string) {
 
 function isLetter(text: string) {
     return text.toString().toLowerCase() != text.toString().toUpperCase();
+}
+
+function findSentencePart(message: string[], index: number, direction?: number): [string[], number] {
+    const sentence = [message[index]];
+    if (direction === undefined)
+        direction = Math.random() > 0.5 ? 1 : -1;
+    if (index == 0)
+        direction = 1;
+    if (index == message.length - 1)
+        direction = -1;
+
+    while (sentence.length < 5 + Math.floor(Math.random() * 5)) {
+        index += direction;
+        if (index < 0 || index >= message.length)
+            break;
+        const word = message[index];
+        if (TOP_WORDS.includes(word) && sentence.length > 3)
+            break;
+        if (word == '.' && sentence.length > 3)
+            break;
+        if (word != '.')
+            if (direction > 0)
+                sentence.push(word);
+            else
+                sentence.unshift(word);
+    }
+    return [sentence, direction];
 }
